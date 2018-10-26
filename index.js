5994673486b178a88a7a9645221f30c7229a312f
Dan Hannigan <hannigan.d92@gmail.com>
2018-10-26 19:50:46 +0100

    Add iOS fallbacks

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-import { requireNativeModules } from "react-native";
+// ImageView.js
+import PropTypes from "prop-types";
+import { View, requireNativeComponent, Platform } from "react-native";
 
-module.exports = requireNativeModules("RNSuperView");
+/**
+ * Composes `View`.
+ *
+ * - src: string
+ * - borderRadius: number
+ * - resizeMode: 'cover' | 'contain' | 'stretch'
+ */
+if (Platform.OS === "android") {
+  module.exports = requireNativeComponent("RCTSuperView", {
+    propTypes: {
+      color: PropTypes.string,
+      angle: PropTypes.number,
+      distance: PropTypes.number,
+      radius: PropTypes.number,
+      ...View.propTypes
+    }
+  });
+} else {
+  module.exports = View;
+}
